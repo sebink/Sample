e3463d6b72b9377d58628a623ccfbcdf7923d003
sebink <sebink@gmail.com>
2014-02-06 16:23:07 -0500

    Update my_test.rb

--- my_test.rb
+++ my_test.rb
@@ -32,7 +32,7 @@ describe "LandingPageChecking" do
     @verification_errors = []
 =end
 #............................For Local run ..........................
-    Selenium::WebDriver::Firefox::Binary.path = "/usr/bin/firefox"
+    Selenium::WebDriver::Firefox::Binary.path = "/usr/bin/firefox/firefox-bin"
   @driver = Selenium::WebDriver.for :firefox
     #@driver = Selenium::WebDriver.for :firefox
     #headless = Headless.new
