1ba2dace3b8f87639c1a8d73f1f7fceac260dd23
sebink <sebink@gmail.com>
2014-02-06 15:55:44 -0500

    Update my_test.rb

--- my_test.rb
+++ my_test.rb
@@ -1,7 +1,7 @@
 require "json"
 require 'rubygems'
 #gem 'selenium-webdriver', '2.25.0'
-require "selenium-webdriver", '2.25.0'
+require "selenium-webdriver"
 require "rspec"
 require 'headless'
 include RSpec::Expectations
