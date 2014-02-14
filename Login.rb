683303fb929ca8350bb01d5647fe6355533683fb
sebink <sebink@gmail.com>
2014-02-14 13:55:54 -0500

    Update Login.rb

--- Login.rb
+++ Login.rb
@@ -8,35 +8,20 @@ describe "Login" do
 
   before(:all) do
 
-#...........................For SauceLab..........................
-=begin
-    caps = Selenium::WebDriver::Remote::Capabilities.firefox
+caps = Selenium::WebDriver::Remote::Capabilities.firefox
     caps['platform'] = "Windows 8.1"
     caps['version'] = "26"
-    caps[:name] = "Login"
+    caps[:name] = "Advertiser Campaign "
 
     @driver = Selenium::WebDriver.for(
         :remote,
         :url => "http://btsauce:3d284ce4-ce68-4128-acc2-da28928ff141@ondemand.saucelabs.com:80/wd/hub",
         :desired_capabilities => caps)
-
-    @base_url = "http://uat-portal.blutrumpet.com/"
-    @accept_next_alert = true
-    @driver.manage.timeouts.implicit_wait = 30
-    @verification_errors = []
-
-
-=end
-
-#............................For Local run ..........................
-
-    @driver = Selenium::WebDriver.for :firefox
-    @driver.manage().window().maximize()
-
-    @base_url = "http://uat-portal.blutrumpet.com/"
-    @accept_next_alert = true
-    @driver.manage.timeouts.implicit_wait = 30
-    @verification_errors = []
+        
+   @base_url = "http://uat-portal.blutrumpet.com/"
+   @accept_next_alert = true
+   @driver.manage.timeouts.implicit_wait = 30
+   @verification_errors = []
     
 
 
