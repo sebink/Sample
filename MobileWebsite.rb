ef347fb91738552fff1008c8ccdefb349a21d12d
sebink <sebink@gmail.com>
2014-02-14 14:04:56 -0500

    Update MobileWebsite.rb

--- MobileWebsite.rb
+++ MobileWebsite.rb
@@ -9,7 +9,7 @@ describe 'Login' do
 caps = Selenium::WebDriver::Remote::Capabilities.firefox
     caps['platform'] = "Windows 8.1"
     caps['version'] = "26"
-    caps[:name] = "Advertiser Campaign "
+    caps[:name] = "Mobile Web Site"
 
     @driver = Selenium::WebDriver.for(
         :remote,
