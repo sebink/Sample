8c907ab548c598e4f60580a88851c389727b765c
sebink <sebink@gmail.com>
2014-02-14 13:55:08 -0500

    Update App.rb

--- App.rb
+++ App.rb
@@ -11,12 +11,26 @@ describe 'App' do
   # Init Test
   before(:all) do
 
-    @driver = Selenium::WebDriver.for :firefox
-    @driver.manage().window().maximize()
-    @base_url = 'http://uat-portal.blutrumpet.com/'
-    @accept_next_alert = true
-    @driver.manage.timeouts.implicit_wait = 30
-    @verification_errors = []
+   # @driver = Selenium::WebDriver.for :firefox
+  #  @driver.manage().window().maximize()
+  #  @base_url = 'http://uat-portal.blutrumpet.com/'
+   # @accept_next_alert = true
+  #  @driver.manage.timeouts.implicit_wait = 30
+  #  @verification_errors = []
+  caps = Selenium::WebDriver::Remote::Capabilities.firefox
+    caps['platform'] = "Windows 8.1"
+    caps['version'] = "26"
+    caps[:name] = "Advertiser Campaign "
+
+    @driver = Selenium::WebDriver.for(
+        :remote,
+        :url => "http://btsauce:3d284ce4-ce68-4128-acc2-da28928ff141@ondemand.saucelabs.com:80/wd/hub",
+        :desired_capabilities => caps)
+        
+   @base_url = "http://uat-portal.blutrumpet.com/"
+   @accept_next_alert = true
+   @driver.manage.timeouts.implicit_wait = 30
+   @verification_errors = []
 =begin
     caps = Selenium::WebDriver::Remote::Capabilities.firefox
     caps['platform'] = "Windows 8.1"
